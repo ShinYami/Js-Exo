93f850b09a6ef34a311ca1d5bce4959611212639
Conti Loris <loriisconti@gmail.com>
2021-02-02 18:20:50 +0100

    Update script.js

--- 05-arrays/09-rand-array-stats/script.js
+++ 05-arrays/09-rand-array-stats/script.js
@@ -11,16 +11,31 @@
             i++;
         }
 
-        document.getElementById("n-1").innerHTML = (array[1]);
+        // Need to find another way
+        //document.getElementById("n-1").innerHTML = (array[0]);
+        //document.getElementById("n-2").innerHTML = (array[1]);
+        //document.getElementById("n-3").innerHTML = (array[2]);
+        //document.getElementById("n-4").innerHTML = (array[3]);
+        //document.getElementById("n-5").innerHTML = (array[4]);
+        //document.getElementById("n-6").innerHTML = (array[5]);
+        //document.getElementById("n-7").innerHTML = (array[6]);
+        //document.getElementById("n-8").innerHTML = (array[7]);
+        //document.getElementById("n-9").innerHTML = (array[8]);
+        //document.getElementById("n-10").innerHTML = (array[9]);
 
+        // Second option 
+        let box = 1;
+        while (box <= 10) {
+            document.getElementById("n-${box}").innerHTML = array[box - 1];
+            box++;
+        }
 
+        // just to check in console 
         console.log(array);
-
-
 
         document.getElementById("min").innerHTML = Math.min(...array);
         document.getElementById("max").innerHTML = Math.max(...array);
         document.getElementById("sum").innerHTML = array.reduce((a, b) => a + b, 0);
-        document.getElementById("average").innerHTML = (Math.floor(array.reduce((a, b) => a + b, 0)) / array.length);
+        document.getElementById("average").innerHTML = Math.round(array.reduce((a, b) => a + b, 0)) / 10;
     })
 })();
