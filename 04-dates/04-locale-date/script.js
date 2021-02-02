edfc86ea85ac206cd8866fab89f6720a64292bb9
Conti Loris <loriisconti@gmail.com>
2021-02-02 14:49:16 +0100

    update exo

--- 04-dates/04-locale-date/script.js
+++ 04-dates/04-locale-date/script.js
@@ -7,8 +7,37 @@
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
     // your code here
-    let current = new Date();
-
-    document.getElementById("target").innerHTML = current;
-
+    let d = new Date();
+    let year = d.getFullYear();
+    let date = d.getDate();
+    const months = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+        'July',
+        'August',
+        'September',
+        'October',
+        'November',
+        'December'
+    ];
+    const monthIndex = d.getMonth();
+    const monthName = months[monthIndex];
+    const days = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saterday'
+    ];
+    const dayName = days[d.getDay()];
+    let hour = d.getHours();
+    let minute = d.getMinutes();
+    const formatted = `${dayName} ${date} ${monthName} ${year}, ${hour}h${minute}`;
+    document.getElementById("target").innerHTML = formatted;
 })();
--- 04-dates/05-get-spooky-fridays/script.js
+++ 04-dates/05-get-spooky-fridays/script.js
@@ -5,5 +5,20 @@
 (() => {
 
     // your code here
+    document.getElementById("run").addEventListener("click", () => {
 
+        let year = document.getElementById("year").value;
+        let monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        let i = 0;
+        let months = "";
+
+        while (i < 12) {
+            let date = new Date(year, i, 13);
+            if (date.getDay() === 5) {
+                months = months + " " + monthNames[date.getMonth()];
+            }
+            i++
+        }
+        alert(months);
+    });
 })();
