016af470466ff2ae78100d5fc105cb94bc98ea6a
Conti Loris <loriisconti@gmail.com>
2021-02-03 17:10:43 +0100

    update exo

--- 07-classes/04-getter-setter/script.js
+++ 07-classes/04-getter-setter/script.js
@@ -3,4 +3,27 @@
 
 (() => {
     // your code here
+    class Person {
+        constructor(firstName, lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+        getname() {
+            return `${this.firstName} ${this.lastName}`
+        }
+        setname(newName) {
+            this.firstName = newName.split(' ')[0]
+            this.lastName = newName.split(' ')[1]
+        }
+    }
+
+    document.getElementById('run').addEventListener('click', () => {
+        const Me = new Person('Мандзюк', 'Анна');
+        console.log(Me.getname());
+        Me.setname('Bruel Patrick');
+        console.log(Me.getname());
+    })
+
 })();
+
+//document.getElementById("run").addEventListener('click', () => {});
