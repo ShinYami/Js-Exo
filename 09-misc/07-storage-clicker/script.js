e3d4960d712d63a0dcccd2edf5cb7c45cc6e7436
ShinYami <loriisconti@gmail.com>
2021-02-05 09:38:08 +0100

    update exo storage

--- 09-misc/07-storage-clicker/script.js
+++ 09-misc/07-storage-clicker/script.js
@@ -5,4 +5,21 @@
 
     // your code here
 
+    let increment = document.getElementById('increment');
+
+    let target = document.getElementById('target'); //pas obligé de def une variable pour target
+
+    let count = localStorage.getItem('number');
+    if ( count === undefined){
+        count = 0;
+    }
+
+    target.innerText = count;
+
+    increment.addEventListener('click', () =>{
+        count++;
+        target.innerText = count;
+        localStorage.setItem('number', count);
+    })
+
 })();
