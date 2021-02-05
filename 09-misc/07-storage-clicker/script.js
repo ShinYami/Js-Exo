5067082f1fd1f9c940320cbfc22bd50e00a1b8dd
ShinYami <loriisconti@gmail.com>
2021-02-05 09:41:48 +0100

    update exo

--- 09-misc/07-storage-clicker/script.js
+++ 09-misc/07-storage-clicker/script.js
@@ -5,10 +5,7 @@
 
     // your code here
 
-    let increment = document.getElementById('increment');
-
     let target = document.getElementById('target'); //pas obligé de def une variable pour target
-
     let count = localStorage.getItem('number');
     if ( count === undefined){
         count = 0;
@@ -16,10 +13,12 @@
 
     target.innerText = count;
 
-    increment.addEventListener('click', () =>{
+    document.getElementById("increment").addEventListener("click", () => {
         count++;
         target.innerText = count;
         localStorage.setItem('number', count);
     })
+
+    // localStorage.clear(); (clear)
 
 })();
